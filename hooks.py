0abb4c5181d5ec8d01c2ff62c21e5c3c5ac74965
Mark Hale <mark.hale@physics.org>
2024-09-13 13:28:40 +0100

    Add custom charging hook.

--- hooks.py
+++ hooks.py
@@ -11,3 +11,41 @@ class CustomNameHook:
 
     def update_service_value(self, dbusServiceName, dbusPath, value):
         return []
+
+
+class CustomChargingHook:
+    def __init__(self, service_name, merger, ccls=None, dcls=None):
+        self.service_name = service_name
+        self.merger = merger
+        self.ccls = ccls
+        self.dcls = dcls
+
+    def init_values(self, api):
+        return []
+
+    def update_service_value(self, dbusServiceName, dbusPath, value):
+        if dbusPath == "/Dc/0/Voltage":
+            voltage = self.merger.get_value(dbusPath)
+            paths_changed = []
+
+            ccl = None
+            if self.ccls:
+                for v, cl in self.ccls.items():
+                    if voltage >= float(v):
+                        ccl = float(cl)
+            if ccl is not None:
+                self.merger.update_service_value(self.service_name, "/Info/MaxChargeCurrent", ccl)
+                paths_changed.append("/Info/MaxChargeCurrent")
+
+            dcl = None
+            if self.dcls:
+                for v, cl in self.dcls.items():
+                    if voltage >= float(v):
+                        dcl = float(cl)
+            if dcl is not None:
+                self.merger.update_service_value(self.service_name, "/Info/MaxDischargeCurrent", dcl)
+                paths_changed.append("/Info/MaxDischargeCurrent")
+
+            return paths_changed
+        else:
+            return []

ee7812548bead1c579f0a8c366a886865525177a
Mark Hale <mark.hale@physics.org>
2026-05-02 23:19:02 +0100

    Fixes #113.

--- hooks.py
+++ hooks.py
@@ -27,31 +27,33 @@ class FixedValueHook(Hook):
         return []
 
 
-class CustomChargingHook(Hook):
-    def __init__(self, service_name, merger, ccls=None, dcls=None):
+class AbstractChargingHook(Hook):
+    def __init__(self, value_path, service_name, merger, ccls=None, dcls=None):
+        self.value_path = value_path
         self.service_name = service_name
         self.merger = merger
         self.ccls = ccls
         self.dcls = dcls
 
     def init_values(self):
-        initial_voltage = self.merger.get_value("/Dc/0/Voltage")
-        return self.update_cls(initial_voltage) if initial_voltage is not None else []
+        initial_value = self.merger.get_value(self.value_path)
+        return self.update_cls(initial_value) if initial_value is not None else []
 
     def update_service_value(self, dbusServiceName, dbusPath, value):
-        if dbusPath == "/Dc/0/Voltage":
-            voltage = self.merger.get_value(dbusPath)
-            return self.update_cls(voltage)
+        if dbusPath == self.value_path:
+            merged_value = self.merger.get_value(dbusPath)
+            return self.update_cls(merged_value)
         else:
             return []
 
-    def update_cls(self, voltage):
+    def update_cls(self, value):
+        '''Update charging limits'''
         paths_changed = []
 
         ccl = None
         if self.ccls:
             for v, cl in self.ccls.items():
-                if voltage >= float(v):
+                if value >= float(v):
                     ccl = float(cl)
         if ccl is not None:
             self.merger.update_service_value(self.service_name, "/Info/MaxChargeCurrent", ccl)
@@ -60,10 +62,20 @@ class CustomChargingHook(Hook):
         dcl = None
         if self.dcls:
             for v, cl in self.dcls.items():
-                if voltage >= float(v):
+                if value >= float(v):
                     dcl = float(cl)
         if dcl is not None:
             self.merger.update_service_value(self.service_name, "/Info/MaxDischargeCurrent", dcl)
             paths_changed.append("/Info/MaxDischargeCurrent")
 
         return paths_changed
+
+
+class CustomChargingHook(AbstractChargingHook):
+    def __init__(self, service_name, merger, ccls=None, dcls=None):
+        super().__init__("/Dc/0/Voltage", service_name, merger, ccls=ccls, dcls=dcls)
+
+
+class TemperatureChargingHook(AbstractChargingHook):
+    def __init__(self, service_name, merger, ccls=None, dcls=None):
+        super().__init__("/Dc/0/Temperature", service_name, merger, ccls=ccls, dcls=dcls)
